36bb1b7576ffd4fca1757205491abb6cc224065f
Felipe Salles <prof.felipe.salles@gmail.com>
2022-04-26 01:04:12 -0300

    create the microservices

---
+++ ms-party-hall/src/main/java/com/felipesalles/mspartyhall/MsPartyHallApplication.java
@@ -0,0 +1,13 @@
+package com.felipesalles.mspartyhall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPartyHallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPartyHallApplication.class, args);
+	}
+
+}
