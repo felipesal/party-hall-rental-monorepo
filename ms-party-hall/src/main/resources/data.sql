4e37aabe003a8c2ece223358c141f50fadb7f4b7
Felipe Salles <prof.felipe.salles@gmail.com>
2022-05-01 00:29:39 -0300

    cria data.sql para ms-party-hall

---
+++ ms-party-hall/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_party_hall (NAME, CAPACITY, VALUE_PER_GUEST) VALUES ('Itamaraty Hall', 230, 100.0);
+INSERT INTO tb_party_hall (NAME, CAPACITY, VALUE_PER_GUEST) VALUES ('Le Buffet', 180, 130.0);
+INSERT INTO tb_party_hall (NAME, CAPACITY, VALUE_PER_GUEST) VALUES ('Choupana do guadala', 120, 35.0);
