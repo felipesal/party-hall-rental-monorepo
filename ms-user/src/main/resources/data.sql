7ca983f7350cafa0af6d9d8a0aafedce6e356d39
Felipe Salles <prof.felipe.salles@gmail.com>
2022-05-01 00:28:31 -0300

    gera arquivo data.sql para ms-user

---
+++ ms-user/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TB_USER (NAME, EMAIL) VALUES ('Pedro', 'pedro@mail.com');
+INSERT INTO TB_USER (NAME, EMAIL) VALUES ('Ana', 'ana@mail.com');
+INSERT INTO TB_USER (NAME, EMAIL) VALUES ('Paulo', 'paulo@mail.com');
