92a988ca740c40e8d46895cb57e26d283b3776d1
Felipe Salles <prof.felipe.salles@gmail.com>
2022-05-22 00:57:49 -0300

    adiciona controller ms-user

--- ms-user/src/main/java/com/felipesalles/msuser/controller/UserController.java
+++ ms-user/src/main/java/com/felipesalles/msuser/controller/UserController.java
@@ -6,16 +6,12 @@ import java.util.List;
 import com.felipesalles.msuser.entities.User;
 import com.felipesalles.msuser.services.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -48,6 +44,7 @@ public class UserController {
     @PostMapping
     public ResponseEntity<Void> createUser(@RequestBody User user) {
         User newUser = userService.createUser(user);
+        log.info("Recebido!");
         
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(newUser.getId()).toUri();
